517470bbc47f49b936bffaa53e18961f3b2955f0
Meron Solomon <meronsolomondana@gmail.com>
2025-06-07 16:57:29 +0300

    add card component

---
+++ alx-project-0x02/components/common/Card.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { CardProps} from "@/interfaces";
+
+const Card: React.FC<CardProps> = ({title, content}) => {
+    return (
+        <div className="border border-gray-300 rounded-md p-4 m-4 bg-white text-black">
+            <h2 className="text-xl font-bold mb-2">{title}</h2>
+            <p>{content}</p>
+        </div>
+    )
+}
+
+export default Card;
--- alx-project-0x02/pages/home.tsx
+++ alx-project-0x02/pages/home.tsx
@@ -1,9 +1,13 @@
 import React from "react";
+import Card from "@/components/common/Card"
 
 const HomePage = () => {
   return (
-    <div>
-      <h1 className="text-center p-4">Welcome Home Meriye!</h1>
+    <div className="p-4">
+      <h1 className="text-center text-2xl mb-4 font-bold">Welcome Home Meriye!</h1>
+      <Card title="Card One" content="This is the content of the first card" />
+      <Card title="Card Two" content="This is the content of the second card" />
+      <Card title="Card Three" content="This is the content of the third card" />
     </div>
   );
 };
