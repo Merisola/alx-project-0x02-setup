3ba237cf6ae087e5f9d1d154f9503cb490d454a1
Meron Solomon <meronsolomondana@gmail.com>
2025-06-07 17:40:36 +0300

    create and use button component

---
+++ alx-project-0x02/components/common/Button.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { ButtonProps } from "@/interfaces";
+
+const Button: React.FC<ButtonProps> = ({ label, size, shape, onClick }) => {
+  // Set size classes
+  const sizeClasses =
+    size === "small"
+      ? "px-3 py-1 text-sm"
+      : size === "medium"
+      ? "px-4 py-2 text-base"
+      : "px-5 py-3 text-lg";
+
+  // Combine final classes
+  const className = `bg-blue-500 text-white ${sizeClasses} ${shape} hover:bg-blue-600 transition`;
+
+  return (
+    <button className={className} onClick={onClick}>
+      {label}
+    </button>
+  );
+};
+
+export default Button;
--- alx-project-0x02/pages/about.tsx
+++ alx-project-0x02/pages/about.tsx
@@ -1,10 +1,13 @@
 import React from "react";
+import Button from "@/components/common/Button";
 
 const AboutPage = () => {
   return (
-    <div>
-      <h1>Welcome to the About Page!</h1>
-      <p>This is the home page of our Next.js project.</p>
+    <div className="p-4 space-y-4">
+      <h1 className="text-2xl  font-bold mb-4">Welcome to the About Page!</h1>
+      <Button label="Small Button" size="small" shape="rounded-sm" />
+      <Button label="Medium Button" size="medium" shape="rounded-md" />
+      <Button label="Large Button" size="large" shape="rounded-full" />
     </div>
   );
 };
