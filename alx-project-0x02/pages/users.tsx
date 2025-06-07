529043e18d0972c9163977b3e71c543e2c729484
Meron Solomon <meronsolomondana@gmail.com>
2025-06-07 21:23:16 +0300

    update the user card again

--- alx-project-0x02/pages/users.tsx
+++ alx-project-0x02/pages/users.tsx
@@ -1,3 +1,4 @@
+// pages/users.tsx
 import React from "react";
 import { UserProps } from "@/interfaces";
 import UserCard from "@/components/common/UserCard";
@@ -17,6 +18,7 @@ const UsersPage: React.FC<UsersPageProps> = ({ users }) => {
   );
 };
 
+// this must be at top-level and exported
 export const getStaticProps = async () => {
   const res = await fetch("https://jsonplaceholder.typicode.com/users");
   const data = await res.json();
